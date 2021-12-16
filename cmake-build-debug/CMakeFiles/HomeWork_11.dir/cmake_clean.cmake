f7837b3e4265879850fdf3a2a75e670d5295ffb7
alah35 <maxnet663@gmail.com>
2021-12-16 19:19:22 +0300

    Task 2 (begin)

---
+++ cmake-build-debug/CMakeFiles/HomeWork_11.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HomeWork_11.dir/helper.cpp.obj"
+  "CMakeFiles/HomeWork_11.dir/helper.cpp.obj.d"
+  "CMakeFiles/HomeWork_11.dir/main.cpp.obj"
+  "CMakeFiles/HomeWork_11.dir/main.cpp.obj.d"
+  "HomeWork_11.exe"
+  "HomeWork_11.exe.manifest"
+  "HomeWork_11.pdb"
+  "libHomeWork_11.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HomeWork_11.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/HomeWork_11.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for HomeWork_11.
+# This may be replaced when dependencies are built.
